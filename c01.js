015f5d472f7779faf314a057fe235246fe4ad629
macFauzi <macfauzi@gmail.com>
2024-01-30 23:14:43 +0700

    challenge01

---
+++ c01.js
@@ -0,0 +1,7 @@
+function sum(...numbers) {
+    return numbers.reduce((accumulator, currentValue) => accumulator + currentValue);
+}
+console.log(sum(1, 2, 7));
+console.log(sum(1, 4));
+console.log(sum(11));
+console.log(sum(10, 3, 6, 7, 9));
