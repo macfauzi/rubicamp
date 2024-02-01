d87f5cc6646f6151e508db4fe1f6297079067dd8
macFauzi <macfauzi@gmail.com>
2024-02-01 08:54:50 +0700

    refactor function

--- c01.js
+++ c01.js
@@ -1,8 +1,8 @@
 function sum(...numbers) {
     //write code here
-    return numbers.reduce((accumulator, currentValue) => accumulator + currentValue);
+    console.log(numbers.reduce((accumulator, currentValue) => accumulator + currentValue));
 }
-console.log(sum(1, 2, 7));
-console.log(sum(1, 4));
-console.log(sum(11));
-console.log(sum(10, 3, 6, 7, 9));
+sum(1, 2, 7);
+sum(1, 4);
+sum(11);
+sum(10, 3, 6, 7, 9);
