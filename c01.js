07197aabd3838397eb330d6a9643eb4170074733
macFauzi <macfauzi@gmail.com>
2024-02-01 15:06:20 +0700

    modify c01 & add c03

--- c01.js
+++ c01.js
@@ -1,7 +1,48 @@
-function sum(...numbers) {
-    //write code here
-    console.log(numbers.reduce((accumulator, currentValue) => accumulator + currentValue));
+// function sum(...numbers) {
+//     //write code here
+//     console.log(numbers.reduce((accumulator, currentValue) => accumulator + currentValue));
+// }
+// sum(1, 2, 7);
+// sum(1, 4);
+// sum(11);
+// sum(10, 3, 6, 7, 9);
+
+// alternatif02 dengan menggunakan Array.from() yang merubah arguments menjadi array
+
+// function sum() {
+//     //write code here
+//     console.log(Array.from(arguments).reduce((accumulator, currentValue) => accumulator + currentValue));
+// }
+
+// sum(1, 2, 7);
+// sum(1, 4);
+// sum(11);
+// sum(10, 3, 6, 7, 9);
+
+
+// alternatif03 dengan menggunakan Object.values() yang mengambil valuesnya
+
+// function sum() {
+//     //write code here
+//     console.log(Object.values(arguments).reduce((accumulator, currentValue) => accumulator + currentValue));
+// }
+
+// sum(1, 2, 7);
+// sum(1, 4);
+// sum(11);
+// sum(10, 3, 6, 7, 9);
+
+// alternatif04 cara pemula dengan menggunakan for loop
+
+function sum() {
+    // your code below
+    let hasil = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        hasil += arguments[i];
+    }
+    console.log(hasil);
 }
+
 sum(1, 2, 7);
 sum(1, 4);
 sum(11);
---
+++ c03.js
@@ -0,0 +1,24 @@
+function romawi(n) {
+    //write code here
+    const nDecimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const simbolRomawi = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+    let angkaRomawi = "";
+
+    for (let i = 0; i < nDecimals.length; i++) {
+        while (n >= nDecimals[i]) {
+            angkaRomawi += simbolRomawi[i];
+            n -= nDecimals[i];
+        }
+    }
+    return angkaRomawi;
+}
+
+console.log("Script Testing untuk Konversi Romawi\n");
+console.log("input | expected | result");
+console.log("------|----------|-------");
+console.log("4     | IX       | ", romawi(4));
+console.log("9     | IX       | ", romawi(13));
+console.log("13    | XIII     | ", romawi(13));
+console.log("1453  | MCDLIII  | ", romawi(1453));
+console.log("1646  | MDCXLVI  | ", romawi(1646));
