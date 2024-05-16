fb7c5317655e095e554b240e3c9864d9ceefcb8a
macFauzi <macfauzi@gmail.com>
2024-05-16 13:40:18 +0700

    challenge17

---
+++ c17/Calculator.js
@@ -0,0 +1,47 @@
+export const PI = 22 / 7
+
+export default class Calculator {
+    constructor(x = 1) {
+        this.x = x;
+    }
+
+    add(value) {
+        this.x += value;
+        return this;
+    }
+
+    substract(value) {
+        this.x -= value;
+        return this;
+    }
+
+    divide(value) {
+        this.x /= value;
+        return this;
+    }
+
+    multiply(value) {
+        this.x *= value;
+        return this;
+    }
+
+    square() {
+        this.x = this.x ** 2;
+        return this;
+    }
+
+    exponent(value) {
+        this.x = Math.pow(this.x, value);
+        return this;
+    }
+
+    squareRoot() {
+        this.x = Math.sqrt(this.x);
+        return this;
+    }
+
+    result() {
+        console.log(this.x);
+        return this;
+    }
+}
